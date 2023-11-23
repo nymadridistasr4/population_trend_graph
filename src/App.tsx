e4700a94e3f4d2de9f95199dcd07bcb66f82a55d
なおくん <naoyayokogawa0513@gmail.com>
2023-11-23 20:36:28 +0900

    change title

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <header style={{ textAlign: 'center' }}>
-        <h1>都道府県別総人口推移グラフ</h1>
+        <h1>都道府県別人口推移グラフ</h1>
       </header>
       <Main />
     </div>
